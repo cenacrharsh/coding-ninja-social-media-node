f1bfc8fc82899f736752b9604814752b8a1322af
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-02-01 14:11:15 +0530

    created view for home and user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,9 @@
 module.exports.home = function (req, res) {
   console.log(`Printing from controllers/home_controller.js`);
 
-  return res.end("<h1>Express is up for Codeial!</h1>");
+  return res.render("home", {
+    title: "Home",
+  });
 };
 
 module.exports.about = function (req, res) {
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,11 +1,15 @@
 module.exports.profile = function (req, res) {
   console.log(`Printing from controllers/users_controller.js`);
 
-  return res.end("<h1>User Profile</h1>");
+  return res.render("user", {
+    title: "User Profile",
+  });
 };
 
 module.exports.post = function (req, res) {
   console.log(`Printing from controllers/users_controller.js`);
 
-  return res.end("<h1>User Post</h1>");
+  return res.render("user", {
+    title: "User Post",
+  });
 };
