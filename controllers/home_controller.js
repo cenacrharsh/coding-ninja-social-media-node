86b4488cf854c532791e9d3fec3f3e69cb1af40f
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-02-06 17:03:44 +0530

    self added code for list of all posts on home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,23 @@
-module.exports.home = function (req, res) {
-  console.log(`Printing from controllers/home_controller.js`);
+//# Importing Model
+const User = require("../models/user");
+const Post = require("../models/post");
 
-  return res.render("home", {
-    title: "Home",
+module.exports.home = function (req, res) {
+  const posts = Post.find({}, function (err, post) {
+    if (err) {
+      console.log("Error in retireving posts");
+      return;
+    }
+
+    //- post.map((p) => console.log(p.content));
+
+    return res.render("home", {
+      title: "Home",
+      post: post,
+    });
   });
 };
 
 /*
- > module.exports.actionName = function(req, res) {}
+> module.exports.actionName = function(req, res) {}
 */
