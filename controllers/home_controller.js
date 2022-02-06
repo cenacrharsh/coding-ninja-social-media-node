958041c94926da0222e1c8227387073bddb2f02a
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-02-06 22:05:09 +0530

    17. Nested Population, Display Cmnt & related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -18,6 +18,12 @@ module.exports.home = function (req, res) {
   //* Prepopulating the user (a referred object in our post Schema) of each post
   Post.find({})
     .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    })
     .exec(function (err, posts) {
       if (err) {
         console.log("Error in retireving posts");
