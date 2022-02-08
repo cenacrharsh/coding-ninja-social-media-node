4675d0ec753151788776adc652d50b6829352331
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-02-08 14:45:57 +0530

    deleting a post authorized

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 //# Importing Post Schema
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 //! Action to submit the data of the post form and save it in the DB
 module.exports.create = function (req, res) {
@@ -17,4 +18,31 @@ module.exports.create = function (req, res) {
       return res.redirect("back");
     }
   );
+};
+
+//! Action to delete a post, and all the comments associated with it
+module.exports.destroy = function (req, res) {
+  //* finding whether the post we are trying to delete, exists in the DB or not
+  Post.findById(req.params.id, function (err, post) {
+    //* checking whether the user who is deleting the post, created the post i.e. is authorized to delete it
+    if (post.user == req.user.id) {
+      post.remove();
+
+      Comment.deleteMany(
+        {
+          post: req.params.id,
+        },
+        function (err) {
+          return res.redirect("back");
+        }
+      );
+    } else {
+      //* if user is not authorized to delete
+      return res.redirect("back");
+    }
+    /*
+    > to compare both need to be in string format, user property stores id of user as per schema and won't contain user object unless populated first so it returns a string id 
+    > .id means converting object id into string - provided by mongoose
+    */
+  });
 };
--- routes/posts.js
+++ routes/posts.js
@@ -11,4 +11,10 @@ const router = express.Router();
 //> checking authentication of user before creating a post, so if user is not signed in it will not reach the action of creating a post
 router.post("/create", passport.checkAuthentication, postsController.create);
 
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  postsController.destroy
+);
+
 module.exports = router;
